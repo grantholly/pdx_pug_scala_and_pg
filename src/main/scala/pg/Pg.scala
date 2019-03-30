eb75c63f786c50966e9400569b71443ff79e037e
grantholly <gdholly@gmail.com>
2019-03-30 15:36:46 -0700

    chaining operations together and final clean up of App entry point

--- src/main/scala/pg/Pg.scala
+++ src/main/scala/pg/Pg.scala
@@ -3,7 +3,7 @@ package pg
 import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
 
@@ -29,15 +29,31 @@ object Pg extends App with PostgresDbClient {
     })
     Await.result(empty, Duration.Inf)
 
-    val chain: Future[_] = db.run(
-      DBIO.seq(
+    val chain: Future[Any] = {
+      val start: Future[Vector[(Int, String, Int)]] = db.run(Queries.selectStar)
+      start.onComplete({
+        case Success(res) => {
+          println("before ETL")
+          println(res)
+        }
+        case Failure(e) =>
+          println(e.printStackTrace())
+          throw new Exception("cannot complete query")
+      })
+      Await.result(start, Duration(1000, MILLISECONDS))
+
+      val etl: Future[_] = db.run(DBIO.seq(
         Queries.insert
           .andThen(Queries.update)
-          .andFinally(Queries.delete)
+          .andFinally(Queries.delete))
       )
-    )
-    Await(chain, Duration(1, "second"))
-
+      etl.onComplete({
+        case Success(res) => res
+        case Failure(e) => e.printStackTrace()
+      })
+      etl
+    }
+    Await.result(chain, Duration(2000, MILLISECONDS))
   } finally db.close()
 }
 
